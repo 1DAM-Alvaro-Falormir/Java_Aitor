2046e18bab47515f892bb1e5a58ab8d3db614800
Aitor <Aitor.gallardo.14@gmail.com>
2022-02-10 19:13:25 +0100

    Correcion ejercicio 3 del apartado A conforme al calculo del perimetro y area

--- Ejercicios 7 Quincena/Ejercicio3_Aitor/src/ejercicioa3_aitor/Ejercicio3_Aitor.java
+++ Ejercicios 7 Quincena/Ejercicio3_Aitor/src/ejercicioa3_aitor/Ejercicio3_Aitor.java
@@ -25,18 +25,18 @@ public class Ejercicio3_Aitor {
         Rectangulo r2 = new Rectangulo();
         
         r1.x1 = 0;
-        r1.x2 = 0;
-        r1.y1 = 5;
+        r1.x2 = 5;
+        r1.y1 = 0;
         r1.y2 = 5;
         r2.x1 = 7;
-        r2.x2 = 9;
-        r2.y1 = 2;
+        r2.x2 = 2;
+        r2.y1 = 9;
         r2.y2 = 3;
         
         System.out.println("Las cordenadas del rectangulo1 son: ("+r1.x1+","+r1.x2+") y ("+r1.y1+","+r1.y2+")");
         System.out.println("Las cordenadas del rectangulo2 son: ("+r2.x1+","+r2.x2+") y ("+r2.y1+","+r2.y2+")");
-        System.out.println("El pemiremetro del rectangulo1 es: "+(r1.x1+r1.x2+r1.y1+r1.y2)+" y del rectangulo2 es: "+(r2.x1+r2.x2+r2.y1+r2.y2));
-        System.out.println("El area del rectangulo1 es: "+((r1.x1+r1.x2)*(r1.y1+r1.y2))+" y del rectangulo2 es: "+((r2.x1+r2.x2)*(r2.y1+r2.y2)));
+        System.out.println("El pemiremetro del rectangulo1 es: "+(2*(r1.x1-r1.x2)+(r1.y1-r1.y2)*2)+" y del rectangulo2 es: "+(2*(r2.x1-r2.x2)+(r2.y1-r2.y2)*2));
+        System.out.println("El area del rectangulo1 es: "+((r1.x1-r1.x2)*(r1.y1+r1.y2))+" y del rectangulo2 es: "+((r2.x1-r2.y1)*(r2.y2-r2.x2)));
         
         r1.x1 = 3;
         r1.x2 = 4;
