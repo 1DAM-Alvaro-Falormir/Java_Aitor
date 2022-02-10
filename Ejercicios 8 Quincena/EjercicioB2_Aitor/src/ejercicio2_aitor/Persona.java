1f777ddfef7c89a3814834edc28fba866f14f3d5
Aitor <Aitor.gallardo.14@gmail.com>
2022-02-10 20:20:27 +0100

    Ejercicio B2 8ª Quincena hecho

---
+++ Ejercicios 8 Quincena/EjercicioB2_Aitor/src/ejercicio2_aitor/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2_aitor;
+
+/**
+ *
+ * @author aitgal
+ */
+public class Persona{
+    String dni;
+    String nombre;
+    String apellidos;
+    int edad;
+    
+    public Persona(String dni, String nombre, String apellidos, int edad){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+}
