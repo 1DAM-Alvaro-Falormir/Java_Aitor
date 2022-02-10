1a535285157e76d21d7b20a42a8f7001d3031ec9
Aitor <Aitor.gallardo.14@gmail.com>
2022-02-10 20:07:05 +0100

    Ejercicios B1 8ª Quincena hecho

---
+++ Ejercicios 8 Quincena/EjercicioB1_Aitor/src/ejercicio1_aitor/Punto.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1_aitor;
+
+/**
+ *
+ * @author Aitor
+ */
+public class Punto {
+    int x;
+    int y;
+    
+    public Punto(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
