55cd0b8dc2936c6ef47f4d5d929633f8cccc04ed
Aitor <Aitor.gallardo.14@gmail.com>
2022-03-30 17:15:02 +0200

    Ejercicio 3 10 Quincena hecho

---
+++ Ejercicios 10 Quincena/Ej3_Aitor/src/ej3_aitor/Ej3_Aitor.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ej3_aitor;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aitor
+ */
+public class Ej3_Aitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] vector = new double[5];
+        
+        for (int i = 0; i < vector.length; i++) {
+            try{
+                System.out.print("Introduzca un entero: ");
+                vector[i] = sc.nextDouble();
+            }
+            catch(InputMismatchException ex){
+                System.out.println("Valor introducido incorrecto.");
+                sc.nextLine();
+                i--;
+            }
+        }
+        
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i]+", ");
+        }
+        
+    }
+    
+}
