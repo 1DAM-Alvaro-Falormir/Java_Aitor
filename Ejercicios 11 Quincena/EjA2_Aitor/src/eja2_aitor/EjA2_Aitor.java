3e35f87e0c8d80974e2169b9ddc6f87b35c98c84
Aitor <Aitor.gallardo.14@gmail.com>
2022-05-02 19:31:37 +0200

    Ejercicio A2 de la Quincena 11 Hecho

--- Ejercicios 11 Quincena/EjA2_Aitor/src/eja2_aitor/EjA2_Aitor.java
+++ Ejercicios 11 Quincena/EjA2_Aitor/src/eja2_aitor/EjA2_Aitor.java
@@ -6,6 +6,8 @@ package eja2_aitor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -19,15 +21,27 @@ public class EjA2_Aitor {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String ruta = "";
+        String ruta = "", detalles;
         File arch;
+        boolean info, ok;
 
         do {
             try {
                 System.out.println("Introduzca una ruta para mostrar su informacion:");
                 ruta = sc.nextLine();
                 arch = new File(ruta);
-                muestraInfoRuta(arch);
+                if (!ruta.equals("")) {
+                    do {
+                        System.out.println("Quieres ver el tamaño de los archivos y su fecha de la última modificacion? Si/No");
+                        detalles = sc.next();
+                        ok = Utilidades.validarSN(detalles);
+                    } while (!ok);
+                    info = detalles.equalsIgnoreCase("si");
+                } else {
+                    info = false;
+                }
+                muestraInfoRuta(arch, info);
+
             } catch (FileNotFoundException e) {
                 if (!ruta.equals("")) {
                     System.err.println("Ruta no encontrada.");
@@ -35,27 +49,56 @@ public class EjA2_Aitor {
             }
 
         } while (!ruta.equals(""));
-        
+
     }
-    
-    public static void muestraInfoRuta(File ruta) throws FileNotFoundException {
+
+    public static void muestraInfoRuta(File ruta, boolean info) throws FileNotFoundException {
         File[] lista;
-        
+        long ms;
+        Date fecha;
+
         if (ruta.exists()) { //Comprobamos que la ruta existe
             if (ruta.isFile()) { //Comprobamos si la ruta es archivo
                 System.out.println("Nombre: " + ruta.getName());
             } else { //Si la ruta es un directorio se añadira a un array de Objetos File,
                 lista = ruta.listFiles();
-                
-                for(File Archivo : lista){
-                    if(Archivo.isDirectory())
-                        System.out.println("[*]Nombre: " + Archivo.getName());
-                    
-                }
-                
-                for(File Archivo : lista){
-                    if(Archivo.isFile())
-                        System.out.println("[A]Nombre: " + Archivo.getName());
+                Arrays.sort(lista);
+
+                if (info) {
+                    for (File Archivo : lista) {
+                        if (Archivo.isDirectory()) {
+                            System.out.println("[*]Nombre: " + Archivo.getName());
+                            System.out.println("Tamaño: " + Archivo.length() + " bytes");
+                            ms = Archivo.lastModified();
+                            fecha = new Date(ms);
+                            System.out.println("Última Modificación: " + fecha);
+
+                        }
+
+                    }
+
+                    for (File Archivo : lista) {
+                        if (Archivo.isFile()) {
+                            System.out.println("[A]Nombre: " + Archivo.getName());
+                            System.out.println("Tamaño: " + Archivo.length() + " bytes");
+                            ms = Archivo.lastModified();
+                            fecha = new Date(ms);
+                            System.out.println("Última Modificación: " + fecha);
+                        }
+                    }
+                } else {
+                    for (File Archivo : lista) {
+                        if (Archivo.isDirectory()) {
+                            System.out.println("[*]Nombre: " + Archivo.getName());
+                        }
+
+                    }
+
+                    for (File Archivo : lista) {
+                        if (Archivo.isFile()) {
+                            System.out.println("[A]Nombre: " + Archivo.getName());
+                        }
+                    }
                 }
 
             }
@@ -64,5 +107,5 @@ public class EjA2_Aitor {
         }
 
     }
-    
+
 }
