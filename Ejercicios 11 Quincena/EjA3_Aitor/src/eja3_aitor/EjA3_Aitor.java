599ac6eb9c485c3b06a086493a42a73cffae3d48
Aitor <Aitor.gallardo.14@gmail.com>
2022-05-02 19:46:29 +0200

    Ejercicio A3 de la Quincena 11 Esqueleto

---
+++ Ejercicios 11 Quincena/EjA3_Aitor/src/eja3_aitor/EjA3_Aitor.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eja3_aitor;
+
+/**
+ *
+ * @author aitgal
+ */
+public class EjA3_Aitor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
