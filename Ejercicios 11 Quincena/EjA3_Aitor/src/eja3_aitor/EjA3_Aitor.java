d0b6ccca0da661edccad3347d669f7a8a14788e9
Aitor <Aitor.gallardo.14@gmail.com>
2022-05-02 20:16:46 +0200

    Ejercicio A3 de la Quincena 11 Hecho

--- Ejercicios 11 Quincena/EjA3_Aitor/src/eja3_aitor/EjA3_Aitor.java
+++ Ejercicios 11 Quincena/EjA3_Aitor/src/eja3_aitor/EjA3_Aitor.java
@@ -4,6 +4,8 @@
  */
 package eja3_aitor;
 
+import java.io.File;
+
 /**
  *
  * @author aitgal
@@ -14,7 +16,45 @@ public class EjA3_Aitor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        File documentos = new File("Documentos");
+        File doc = new File("DOC");
         
+        File fotografias = new File("DOC/Fotografias");
+        File fotos = new File("DOC/FOTOS");
+        
+        File libros = new File("DOC/Libros");
+        File lecturas = new File("DOC/LECTURAS");
+        
+        boolean okF, okL;
+        
+        
+        documentos.renameTo(doc);
+        okF = fotografias.renameTo(fotos);
+        okL = libros.renameTo(lecturas);
+        
+        if(okF){
+            RemoveExtensions(fotos);
+        } else
+            System.out.println("No ha cambiado Fotos");
+        if(okL){
+            RemoveExtensions(lecturas);
+        } else
+            System.out.println("No ha cambiado Libros");
+        
+    }
+    
+    public static void RemoveExtensions(File f){
+        File[] lista = f.listFiles();
+        int indexOf;
+        String noTxt;
+        File rename;
+        
+        for (File arch : lista) {
+            indexOf = arch.getAbsolutePath().indexOf(".");
+            noTxt = arch.getAbsolutePath().substring(0 , indexOf);
+            rename = new File(noTxt);
+            arch.renameTo(rename);
+        }
     }
     
 }
